954cc893a9619503bb2731915b37fd693efa7469
luctc04 <luctcph37171@fpt.edu.vn>
2023-11-18 16:36:50 +0700

    update: thêm, sửa, xóa chức năng quản lí tài khoản.

---
+++ src/Controllers/Admin/TaikhoanController.php
@@ -0,0 +1,69 @@
+<?php 
+
+namespace Ductong\BaseMvc\Controllers\Admin;
+
+use Ductong\BaseMvc\Controller;
+use Ductong\BaseMvc\Models\Taikhoan;
+
+class TaikhoanController extends Controller
+{
+    public function index() {
+        $taikhoans = (new Taikhoan)->all();
+        
+        $this->renderAdmin('taikhoans/index', ['taikhoans' => $taikhoans]);
+    }
+
+    public function create() {
+        if (isset($_POST['btn-submit'])) { 
+            $data = [
+                'user' => $_POST['user'],
+                'pass' => $_POST['pass'],
+                'email' => $_POST['email'],
+                'address' => $_POST['address'],
+                'tel' => $_POST['tel'],
+                'role' => $_POST['role'],
+                
+            ];
+
+            (new Taikhoan)->insert($data);
+
+            header('Location: /admin/taikhoans');
+        }
+
+        $this->renderAdmin('taikhoans/create');
+    }
+    
+    public function update() {
+
+        if (isset($_POST["btn-submit"])) { 
+            $data = [
+                'user' => $_POST['user'],
+                'pass' => $_POST['pass'],
+                'email' => $_POST['email'],
+                'address' => $_POST['address'],
+                'tel' => $_POST['tel'],
+                'role' => $_POST['role'],
+            ];
+
+            $conditions = [
+                ['id', '=', $_GET['id']],
+            ];
+
+            (new Taikhoan())->update($data, $conditions);
+        }
+
+        $taikhoan = (new Taikhoan())->findOne($_GET["id"]);
+
+        $this->renderAdmin("taikhoans/update", ["taikhoan" => $taikhoan]);
+    }
+
+    public function delete() {
+        $conditions = [
+            ['id', '=', $_GET['id']]
+        ];
+
+        (new Taikhoan)->delete($conditions);
+
+        header('Location: /admin/taikhoans');
+    }
+}
