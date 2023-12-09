9de1dbaac569f58c87cd4dd8c5a0fcfe6479658a
luctc04 <luctcph37171@fpt.edu.vn>
2023-12-09 20:20:09 +0700

    update giao diện

--- duan1.sql
+++ duan1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 05, 2023 at 01:57 AM
+-- Generation Time: Dec 09, 2023 at 01:19 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -40,7 +40,7 @@ CREATE TABLE `binhluan` (
 --
 
 INSERT INTO `binhluan` (`id`, `noidung`, `iduser`, `idpro`, `ngaybinhluan`) VALUES
-(17, 'vãi lồng đèn =))\r\n', 5, 51, '15:33:24pm 04/12/2023');
+(19, 'vitamin meo meo\r\n', 31, 64, '13:11:44pm 09/12/2023');
 
 -- --------------------------------------------------------
 
@@ -58,11 +58,9 @@ CREATE TABLE `danhmuc` (
 --
 
 INSERT INTO `danhmuc` (`id`, `name`) VALUES
-(13, 'CHÓ ALASKA MALAMUTE'),
-(14, 'CHÓ GOLDEN RETRIEVER'),
-(15, 'CHÓ HUSKY SIBERIAN'),
-(16, 'MÈO TAI CỤP'),
-(17, 'MÈO CHÂN NGẮN');
+(22, 'Loại Khác'),
+(23, 'Chó'),
+(24, 'Mèo');
 
 -- --------------------------------------------------------
 
@@ -84,18 +82,9 @@ CREATE TABLE `order_detail` (
 --
 
 INSERT INTO `order_detail` (`id_order_detail`, `id_order`, `id_pro`, `giamua`, `soluong`, `thanhtien`) VALUES
-(58, 79, 54, 4000000.00, 2, 8000000.00),
-(59, 79, 52, 8000000.00, 1, 8000000.00),
-(60, 80, 51, 13000000.00, 1, 13000000.00),
-(61, 81, 55, 9000000.00, 1, 9000000.00),
-(62, 82, 54, 4000000.00, 1, 4000000.00),
-(63, 83, 54, 4000000.00, 1, 4000000.00),
-(64, 84, 54, 4000000.00, 1, 4000000.00),
-(65, 85, 54, 4000000.00, 1, 4000000.00),
-(66, 86, 54, 4000000.00, 1, 4000000.00),
-(67, 87, 54, 4000000.00, 1, 4000000.00),
-(68, 88, 54, 4000000.00, 1, 4000000.00),
-(69, 89, 54, 4000000.00, 1, 4000000.00);
+(82, 104, 59, 44444.00, 1, 44444.00),
+(83, 105, 62, 3.00, 2, 6.00),
+(84, 106, 64, 444444.00, 1, 444444.00);
 
 -- --------------------------------------------------------
 
@@ -121,11 +110,12 @@ CREATE TABLE `sanpham` (
 --
 
 INSERT INTO `sanpham` (`id`, `name`, `img`, `soluong`, `price`, `price_sale`, `xuatsu`, `mota`, `luotxem`, `iddm`) VALUES
-(51, 'CHÓ ALASKA MALAMUTE', 'p5.jpg', 15, 15000000.00, 13000000.00, ' Nguồn gốc: nhân giống tại Trang trại Dogily Kennel Đà Lạt (thành viên Hiệp hội những người nuôi chó giống Việt Nam – VKA).', 'Chó Alaska\r\nChó Alaska hay Alaska Malamute hay Mahlemuts xuất thân là giống chó được sử đụng dể kéo xe. Người dân Eskimo ở vùng Alaska đã khám phá ra khả năng di chuyển và kéo xe trượt tuyết của các bé chó này. Nổi bất bởi sự to lớn, khỏe mạnh, bền bỉ và có thể chịu đựng được khí hậu khắc nghiệt vùng Bắc Cực.\r\n\r\nTuy nhiên hiện tại giống chó này đã được nhân giống rộng rãi và được nuôi dưỡng như thú cưng tại rát nhiều quốc gia trên toàn thế giới.\r\n\r\nĐẶC ĐIỂM CỦA CHÓ ALASKA\r\nTheo sự phân chia của AKC (hiệp hội chó Hoa Kỳ), có tất cả ba loại chó Alaska là Standard (tiêu chuẩn), Large Standard (tiêu chuẩn lớn) và Giant (khổng lồ). Có Alaska có độ cao trung bình từ 63,5cm cho đến 68,5cm. Trong đó dòng Alaska Giant có thể cao tới 70cm và nặng tới 75kg.', 0, 13),
-(52, 'HUSKY XÁM', 'p2.jpg', 7, 9000000.00, 8000000.00, '✔️ Nguồn gốc: nhân giống tại Trang trại Dogily Kennel Đà Lạt (thành viên Hiệp hội những người nuôi chó giống Việt Nam – VKA).', 'Đặc điểm ngoại hình Husky\r\nHusky sở hữu một thân hình thuôn dài, to khỏe và rất săn chắc. Chiều cao trung bình từ 53 đến 58 cm và cân nặng từ 16 đến 30 kg (thường những chú “Ngáo” giống đực sẽ có thân hình nhỉnh hơn những con cái). Loài chó này có một bộ lông dày và có 2 lớp giữ ấm (thường rụng lông 2 lần một năm).\r\n\r\nHusky sở hữu một thân hình thuôn dài, to khỏe và rất săn chắc\r\n\r\nMũi của chó Husky đặc biệt hơn so với các giống chó khác vì màu mũi của chúng sẽ cùng tông theo màu lông. Lúc nào mũi của chúng cũng hơi ướt một chút. Vào thời tiết lạnh, mũi Husky sẽ chuyển sang màu nâu hoặc màu hồng nhạt nhưng trong mùa hè thì mũi chúng có màu như ban đầu.', 0, 15),
-(53, 'MÈO ALN BLUE TAI CỤP', 'm10.jpg', 4, 6000000.00, 5500000.00, '', '', 0, 16),
-(54, 'MÈO CHÂN LÙN', 'p1.jpg', 18, 5000000.00, 4000000.00, '', '', 0, 17),
-(55, 'MÈO VÀNG', 'm9.jpg', 5, 11000000.00, 9000000.00, '', '', 0, 16);
+(59, 'Chó Alaska Malamute', 'p5.jpg', 7, 10000000.00, 8000000.00, 'Việt Nam', 'Một số thông tin các em:\r\n- Giống: Alaska Oversize thuần chủng ( cha mẹ trên 50kg)\r\n- Nguồn gốc: sinh sản tại Việt Nam\r\n- Số lượng: 3 cái 4 đực\r\n- Tiêm chủng: Đủ 02 mũi 7 bệnh ( có sổ sức khoẻ)\r\n*Chế độ bảo hành VÀNG tại shop như sau:\r\n1. Bảo hành thuần chủng trọn đời\r\n2. Bảo hành care, parvo 1 tuần (hai loại bệnh phổ biến thường xảy ra khi chó ko được tiêm chủng)', 0, 23),
+(60, 'Mèo Chân Ngắn Tai Cụp', 'p1.jpg', 9, 5000000.00, 4500000.00, 'Nước Ngoài', 'Thông tin về bé\r\nGiống : Anh lông ngắn Munchkin (chân lùn) + TAI CỤP - ĐẢM BẢO THUẦN CHỦNG 100%\r\nGiới tính : Cái\r\nMàu : Bicolor\r\nBé đã tiêm dinh dưỡng và đã sổ giun\r\nBé khoẻ mạnh, linh hoạt và quấn chủ . Bé đã biết tự ăn ( hạt + pate), biết đi vệ sinh đúng chỗ, mèo nhà nuôi chứ không phải dạng trang trại nên khách cứ an tâm ạ.\r\n*Bạn cần xem thêm hình và video bé có thể kết bạn zal0 với mình\r\n*Có thể đến xem và rước bé trực tiếp hoặc mình cũng có ship đi ạ\r\n*Bên mình có hỗ trợ hướng dẫn cách chăm sóc bé tận tình', 0, 24),
+(61, 'Chó Corgi Mông trái tim', 'p12.jpg', 4, 6500000.00, 6000000.00, 'Việt Nam', 'Các con tìm bát ăn mới đây ạ\r\n-Dễ nuôi dễ chăm sóc, ăn cơm cháo hoặc hạt khô ngon lành\r\n-Đã tiêm phòng mũi 5 trong 1 và tẩy giun định kì cho bé\r\nKhuyến khích mọi người qua tận nhà xem cún trực tiếp để lựa chọn ưng ý nha\r\nNhận ship toàn quốc nhận cún thanh toán ạ\r\nAi cần thêm thông tin Ib zallo em hoặc call trực tiếp\r\nGiá cực yêu thương ạ', 0, 23),
+(62, 'Mèo Chân Ngắn Xinh Xinh', 'm3.jpg', 11, 5000000.00, 4500000.00, 'Việt Nam', 'Thông tin về bé Giống : Anh lông ngắn Munchkin (chân lùn) + TAI CỤP - ĐẢM BẢO THUẦN CHỦNG 100% Giới tính : Cái Màu : Bicolor Bé đã tiêm dinh dưỡng và đã sổ giun Bé khoẻ mạnh, linh hoạt và quấn chủ . Bé đã biết tự ăn ( hạt + pate), biết đi vệ sinh đúng chỗ, mèo nhà nuôi chứ không phải dạng trang trại nên khách cứ an tâm ạ. *Bạn cần xem thêm hình và video bé có thể kết bạn zal0 với mình *Có thể đến xem và rước bé trực tiếp hoặc mình cũng có ship đi ạ *Bên mình có hỗ trợ hướng dẫn cách chăm sóc bé tận tình', 0, 24),
+(63, 'Chó samoyed trắng tuyết', 'p9.jpg', 6, 8000000.00, 7500000.00, 'Việt Nam', 'Samoyed là một giống chó có nguồn gốc từ vùng đất Siberia lạnh giá và được gọi với cái tên trìu mến Nàng Bạch Tuyết của rừng Taiga.\r\n- Đôi mắt đen, sâu\r\n- Mũi màu nâu hoặc đen\r\n- Hàm răng khỏe\r\n- Tai có kích thước bình thường, vành tai hơi cong tròn, đỉnh luôn dựng đứng\r\n- Phần đuôi dài, thường cuộn vòng trên lưng\r\n- Bộ lông dày và rụng nhiều vào mùa nóng', 0, 23),
+(64, 'Mèo Scottish tai cụp', 'm9.jpg', 8, 3000000.00, 2500000.00, 'Việt Nam', 'Mèo tai cụp là một giống mèo khá độc đáo với đôi tai cụp không thể lẫn được với bất kì giống mèo nào khác. Chúng đang được những người yêu mèo tìm mua và nhận được sự quan tâm đặc biệt. Khi đôi tai gập đã trở nên khá đặc trưng, những người nghiện mèo lại tìm đến những màu sắc khác lạ. Mèo tai cụp màu vàng hiện đang rất được nhiều người lựa chọn bởi sự bắt mắt và sang trọng.', 0, 24);
 
 -- --------------------------------------------------------
 
@@ -140,7 +130,7 @@ CREATE TABLE `taikhoan` (
   `email` varchar(255) NOT NULL,
   `address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `tel` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `role` tinyint NOT NULL DEFAULT '0'
+  `role` tinyint NOT NULL DEFAULT '0' COMMENT '0.Người dùng\r\n1.Admin\r\n'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -149,9 +139,7 @@ CREATE TABLE `taikhoan` (
 
 INSERT INTO `taikhoan` (`id`, `user`, `pass`, `email`, `address`, `tel`, `role`) VALUES
 (5, 'admin', '123', 'luctcph37171@fpt.edu.vn', 'Bích Hòa, Thanh Oai, Hà Nội', '0332293871', 1),
-(14, 'luc', '123', 'truongcongluc2004@gmail.com', 'Bích Hòa, Thanh Oai, Hà Nội', '0332293871', 0),
-(23, 'lucdzs1tg', '123', 'luc@gmail.com', 'Không có địa chỉ nhà ...!', '0123456789', 0),
-(24, 'thu', '123', 'thuntlph31086@fpt.edu.vn', NULL, NULL, 0);
+(31, 'luc', '123', 'truongcongluc2004@gmail.com', NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -177,17 +165,9 @@ CREATE TABLE `tbl_order` (
 --
 
 INSERT INTO `tbl_order` (`id_order`, `id_user`, `hoten`, `sdt`, `email`, `diachi`, `tongtien`, `pttt`, `ngaydathang`, `trangthai`) VALUES
-(79, 14, 'Trương Công Lực', '0332293871', 'luctcph37171@fpt.edu.vn', 'bích hòa, thanh oai, hà nội ', 16000000.00, 2, '2023-12-04 22:34:33', 3),
-(80, 14, 'test1', '03334498564', 'luc@gmail.com', 'hà đông, hà nội', 13000000.00, 1, '2023-12-04 22:36:25', 1),
-(81, 24, 'thu', '0332293871', 'thuntlph31086@fpt.edu.vn', 'lllllllllllllllllllllllllllllllllll', 9000000.00, 2, '2023-12-05 07:28:24', 1),
-(82, 24, 'luc', '0332293871', 'luctcph37171@fpt.edu.vn', 'cccccccccccc', 4000000.00, 1, '2023-12-05 07:31:08', 1),
-(83, 14, 'Trương Công Lực', '0332293871', 'thuntlph31086@fpt.edu.vn', 'bích hòa, thanh oai, hà nội ', 4000000.00, 1, '2023-12-05 07:34:02', 1),
-(84, 14, 'Trương Công Lực', '0332293871', 'luctcph37171@fpt.edu.vn', 'bích hòa, thanh oai, hà nội ', 4000000.00, 1, '2023-12-05 07:37:12', 1),
-(85, 14, 'thu', '0332293871', 'thuntlph31086@fpt.edu.vn', 'lllllllllllllllllllllllllllllllllll', 4000000.00, 1, '2023-12-05 07:37:58', 1),
-(86, 24, 'thu', '0332293871', 'thuntlph31086@fpt.edu.vn', 'bích hòa, thanh oai, hà nội ', 4000000.00, 1, '2023-12-05 07:38:44', 1),
-(87, 0, 'test3', '0332293871', 'luctcph37171@fpt.edu.vn', 'dddddddddddd', 4000000.00, 1, '2023-12-05 08:10:09', 1),
-(88, 14, 'test5', '0332293871', 'luctcph37171@fpt.edu.vn', 'bích hòa, thanh oai, hà nội ', 4000000.00, 1, '2023-12-05 08:10:35', 1),
-(89, 14, 'test10', '0332293871', 'luctcph37171@fpt.edu.vn', '4fff', 4000000.00, 1, '2023-12-05 08:14:05', 1);
+(104, 31, 'Trương Công Lực', '0332293871', 'luc@gmail.com', 'dddddddddddd', 44444.00, 1, '2023-12-08 18:57:01', 4),
+(105, 0, 'luc', '0332293871', 'luc@gmail.com', 'dddddddddddd', 6.00, 1, '2023-12-09 16:47:00', 1),
+(106, 31, 'fff', '0332293871', 'truongcongluc2004@gmail.com', 'fffffff', 444444.00, 1, '2023-12-09 16:54:17', 1);
 
 --
 -- Indexes for dumped tables
@@ -239,37 +219,37 @@ ALTER TABLE `tbl_order`
 -- AUTO_INCREMENT for table `binhluan`
 --
 ALTER TABLE `binhluan`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `danhmuc`
 --
 ALTER TABLE `danhmuc`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `order_detail`
 --
 ALTER TABLE `order_detail`
-  MODIFY `id_order_detail` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+  MODIFY `id_order_detail` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
 
 --
 -- AUTO_INCREMENT for table `sanpham`
 --
 ALTER TABLE `sanpham`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- AUTO_INCREMENT for table `taikhoan`
 --
 ALTER TABLE `taikhoan`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `tbl_order`
 --
 ALTER TABLE `tbl_order`
-  MODIFY `id_order` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
+  MODIFY `id_order` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
 
 --
 -- Constraints for dumped tables
